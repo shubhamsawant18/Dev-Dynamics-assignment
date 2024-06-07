fb373d9163fc15136858f675dab08d462e26483a
shubhamsawant18 <shubhsawant124@gmail.com>
2024-06-07 14:12:43 +0530

    background

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,22 @@
 
+import { useState } from 'react';
 import './App.css';
 import DataAnalysis from './dashboard';
 
+
 function App() {
+   const [isDark , setDarkMode] = useState(false);
+   
+   const colorDark = { color: 'black', backgroundColor: 'white' };
+const colorWhite = { color: 'white', backgroundColor: 'black' };
+const toggle =()=> {
+  setDarkMode(!isDark)
+}
+
   return (
-    <>
+    <div style={isDark? colorDark: colorWhite}>
       <DataAnalysis/>
-    </>
+    </div>
   );
 }
 
