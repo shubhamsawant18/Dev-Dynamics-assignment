5bd235e4b807d20b79662918510fb702819d477b
shubhamsawant18 <shubhsawant124@gmail.com>
2024-06-07 15:10:36 +0530

    navbar

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,21 +1,26 @@
+// App.tsx
 
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import NavBar from './NavBar';
 import DataAnalysis from './dashboard';
+// Import other components as needed
+import Home from './Home'; // Example import, replace with your components
 
 
 function App() {
-   const [isDark , setDarkMode] = useState(false);
-   
-   const colorDark = { color: 'black', backgroundColor: 'white' };
-const colorWhite = { color: 'white', backgroundColor: 'black' };
-const toggle =()=> {
-  setDarkMode(!isDark)
-}
+  const [isDark, setDarkMode] = useState(false);
+
+  const colorDark = { color: 'black', backgroundColor: 'white' };
+  const colorWhite = { color: 'white', backgroundColor: 'black' };
+  const toggle = () => {
+    setDarkMode(!isDark);
+  };
 
   return (
-    <div style={isDark? colorDark: colorWhite}>
-      <DataAnalysis/>
+    <div style={isDark ? colorDark : colorWhite}>
+   <NavBar/>
+   <DataAnalysis/>
     </div>
   );
 }
